ea264fdbb4a0bdcacdbe94b724f3cce2732e6ac5
ahmad <itxti909@gmail.com>
2024-07-13 21:49:25 +0300

    welocme secreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native'
+import { useRouter } from "expo-router";
+import {
+  View,
+  Text,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+
+import Animated, { FadeIn, FadeInDown, FadeOut } from "react-native-reanimated";
 
 const Index = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Index</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1 flex justify-end ">
+      <StatusBar barStyle={"light-content"} />
+      <Image
+        source={require("../assets/images/welcome.png")}
+        className="h-full w-full absolute "
+      />
+      <View className="h-full w-full absolute bg-black/25 " />
+      <Animated.View 
+      
+      entering={FadeInDown.delay(200).springify()} className="items-center z-10 py-8">
+        <Text className="text-white tracking-wide text-3xl font-bold">
+          Best <Text className="text-rose-500 ">Workouts</Text>
+        </Text>
+        <Text className="text-white tracking-wide text-3xl font-bold">
+          For you
+        </Text>
 
-export default Index
+        <View className="pt-7">
+          <TouchableOpacity onPress={() => {
+            router.push('/home')
+          }} className="w-[80vw] h-14 bg-red-500 rounded-full justify-center items-center ">
+            <Text className="text-2xl text-white font-bold tracking-widest">
+              Get started
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
+    </SafeAreaView>
+  );
+};
+
+export default Index;
---
+++ nativewind.d.ts
@@ -0,0 +1,56 @@
+import "react-native";
+
+declare module "react-native" {
+  interface ViewProps {
+    className?: string;
+  }
+  interface TextProps {
+    className?: string;
+  }
+  interface ImageProps {
+    className?: string;
+  }
+  interface ScrollViewProps {
+    className?: string;
+  }
+  interface TouchableOpacityProps {
+    className?: string;
+  }
+
+  declare module "*.png" {
+    const value: string;
+    export default value;
+  }
+
+  declare module "*.jpg" {
+    const value: string;
+    export default value;
+  }
+
+  declare module "*.jpeg" {
+    const value: string;
+    export default value;
+  }
+
+  declare module "*.gif" {
+    const value: string;
+    export default value;
+  }
+
+  declare module "*.svg" {
+    const content: any;
+    export default content;
+  }
+
+  declare module "*.bmp" {
+    const value: string;
+    export default value;
+  }
+
+  declare module "*.tiff" {
+    const value: string;
+    export default value;
+  }
+
+  // Add similar declarations for other components as needed
+}
